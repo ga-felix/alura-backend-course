dc5fc1466aeb84d87a7e92ec06611af36c239516
ga-felix <gafelix435@gmail.com>
2022-01-12 16:08:03 -0300

    Import model classes from Alura API

---
+++ src/main/java/gafelix/controller/HelloWordController.java
@@ -0,0 +1,14 @@
+package gafelix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWordController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() { return "Hello, world!"; }
+
+}
--- src/main/java/gafelix/mvcbackend/HelloWordController.java
+++
@@ -1,2 +0,0 @@
-package gafelix.mvcbackend;public class HelloWord {
-}
