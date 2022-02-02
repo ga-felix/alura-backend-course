00882de935faaf05fb96399652a6d9b325ddaadf
ga-felix <gafelix435@gmail.com>
2022-02-02 14:13:12 -0300

    Adiciona documentação Swagger

--- src/main/java/gafelix/mvcbackend/config/security/SecurityConfig.java
+++ src/main/java/gafelix/mvcbackend/config/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,6 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.repositories = repositories;
     }
 
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -46,6 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topics/*")
                 .permitAll()
                 .antMatchers(HttpMethod.POST, "/auth")
+                .permitAll()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
